adc771033a8374ae2570d80ea7bf8aaa0b183893
Kirill Belolipetsky <archhaze24@gmail.com>
2022-11-11 23:41:42 +0300

    Updated 0020.

--- js/FIXME0020.js
+++ js/FIXME0020.js
@@ -24,4 +24,38 @@ const isValid = function (s) {
     return true;
 };
 
-console.log(isValid('[)'))
+
+// FIXME: debug the '(){}}{' case
+const isValidFix = function (s) {
+    if (s.length < 2) return false;
+
+    const temporaryArray = s.split('');
+
+    for (let i = 0; i < temporaryArray.length; i++) {
+        if (temporaryArray.length < 2) return false
+
+        let searchingValue;
+
+        if (temporaryArray[0] === '(') {
+            searchingValue = ')';
+        } else if (temporaryArray[0] === '[') {
+            searchingValue = ']';
+        } else if (temporaryArray[0] === '{') {
+            searchingValue = '}';
+        } else searchingValue = undefined;
+
+
+        const index = temporaryArray.findIndex((value, index, array) => {
+            if (index === 0) return searchingValue === array[index + 1]
+            return searchingValue === value;
+        })
+        if (index + 1) {
+            temporaryArray.shift();
+            temporaryArray.splice(index, 1)
+        } else return false;
+    }
+
+    return true;
+};
+
+console.log(isValidFix('(){}}{'))
